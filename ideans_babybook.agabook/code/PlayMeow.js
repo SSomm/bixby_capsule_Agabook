1329798e0273a7f31a19347576c261e534c5fad4
DonghyunJang <jjdh11117@gmail.com>
2019-11-21 14:50:57 +0900

    edit error

--- ideans_babybook.agabook/code/PlayMeow.js
+++ ideans_babybook.agabook/code/PlayMeow.js
@@ -1,15 +1,6 @@
-module.exports.function = function playMeow (songKinds) {
-  result = "기본값"
+module.exports.function = function playMeow () {
 
-  if (songKinds =="뽀로로") {
-    result = "뽀로로"
-  }
-  else if (songKinds == "자장가") {
-    result = "자장가"
-  } 
-  else if (songKinds =="동요") {
-    result = "동요"
-  }
+  result = "기본값";
 
 
   return result;
